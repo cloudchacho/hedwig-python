0fc3f9f92f619fcc5388af4ee32c83eaf5b6d518
Aniruddha Maru <aniruddhamaru@gmail.com>
2018-04-26 13:28:09 -0700

    Bump version: 1.0.12-dev → 1.1.0-dev

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = 'Automatic Labs'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0.12-dev'
+release = '1.1.0-dev'
 
 
 # -- General configuration ---------------------------------------------------
