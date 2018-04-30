7de8abefebe3d5a5663da5344759a42e1fadc66e
Aniruddha Maru <aniruddhamaru@gmail.com>
2018-04-30 15:56:02 -0700

    Bump version: 1.2.0-dev → 1.2.0

--- hedwig/__init__.py
+++ hedwig/__init__.py
@@ -7,7 +7,7 @@ hedwig
 
 
 # semantic versioning (http://semver.org/)
-VERSION = '1.2.0-dev'
+VERSION = '1.2.0'
 
 
 from .consumer import listen_for_messages, process_messages_for_lambda_consumer  # noqa
