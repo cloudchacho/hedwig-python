4ae314e6baf492b1e7a124b2176fbb07184a24bf
Aniruddha Maru <aniruddhamaru@gmail.com>
2018-05-02 09:42:13 -0700

    Bump version: 1.2.1-dev → 1.2.1

--- docs/releases.rst
+++ docs/releases.rst
@@ -1,7 +1,7 @@
 Release Notes
 =============
 
-**Current version: v1.2.1-dev**
+**Current version: v1.2.1**
 
 v1.0
 ~~~~
--- hedwig/__init__.py
+++ hedwig/__init__.py
@@ -7,7 +7,7 @@ hedwig
 
 
 # semantic versioning (http://semver.org/)
-VERSION = '1.2.1-dev'
+VERSION = '1.2.1'
 
 
 from .consumer import listen_for_messages, process_messages_for_lambda_consumer  # noqa
