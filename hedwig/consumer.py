f43a3ea051ce08cd9af774c3657293d9fba28a2c
Aniruddha Maru <aniruddhamaru@gmail.com>
2018-06-04 15:48:50 -0700

    Allow passing a shutdown event that may be used in a SIGTERM handler

--- hedwig/consumer.py
+++ hedwig/consumer.py
@@ -1,6 +1,7 @@
 import json
 import itertools
 import logging
+import threading
 import typing
 
 import boto3
@@ -141,7 +142,7 @@ def process_messages_for_lambda_consumer(lambda_event: dict) -> None:
 
 def listen_for_messages(
         num_messages: int = 10, visibility_timeout_s: typing.Optional[int] = None,
-        loop_count: typing.Optional[int] = None) -> None:
+        loop_count: typing.Optional[int] = None, shutdown_event: threading.Event = None) -> None:
     """
     Starts a Hedwig listener for message types provided and calls the callback handlers like so:
 
@@ -151,16 +152,24 @@ def listen_for_messages(
     kept on queue and processed again. If the callback keeps failing, SQS dead letter queue mechanism kicks in and
     the message is moved to the dead-letter queue.
 
+    This function is blocking by default. It may be run for specific number of loops by passing `loop_count`. It may
+    also be stopped by passing a shut down event object which can be set to stop the function.
+
     :param num_messages: Maximum number of messages to fetch in one SQS API call. Defaults to 10
     :param visibility_timeout_s: The number of seconds the message should remain invisible to other queue readers.
         Defaults to None, which is queue default
     :param loop_count: How many times to fetch messages from SQS. Default to None, which means loop forever.
+    :param shutdown_event: An event to signal that the process should shut down. This prevents more messages from
+        being de-queued and function exits after the current messages have been processed.
     """
+    if not shutdown_event:
+        shutdown_event = threading.Event()
+
     queue_name = get_default_queue_name()
 
     queue = get_queue(queue_name)
     for count in itertools.count():
-        if loop_count is None or count < loop_count:
+        if (loop_count is None or count < loop_count) and not shutdown_event.is_set():
             fetch_and_process_messages(
                 queue_name, queue, num_messages=num_messages, visibility_timeout=visibility_timeout_s)
         else:
