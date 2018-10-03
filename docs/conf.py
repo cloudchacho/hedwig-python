0b98b62ec9b8379bd2512c99f56cc3ffb48c7935
Aniruddha Maru <aniruddhamaru@gmail.com>
2018-10-03 15:36:07 -0700

    Bump version: 3.0.0 → 3.0.1-dev

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = 'Automatic Labs'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '3.0.0'
+release = '3.0.1-dev'
 
 
 # -- General configuration ---------------------------------------------------
