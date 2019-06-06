8696158ce31b933cf157a302fb641ec7de5a2cf1
Aniruddha Maru <aniruddhamaru@gmail.com>
2019-06-06 12:02:42 -0700

    Bump version: 5.0.2-dev → 6.0.0-dev

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = 'Automatic Labs'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '5.0.2-dev'
+release = '6.0.0-dev'
 
 
 # -- General configuration ---------------------------------------------------
--- docs/releases.rst
+++ docs/releases.rst
@@ -1,7 +1,7 @@
 Release Notes
 =============
 
-**Current version: v5.0.2-dev**
+**Current version: v6.0.0-dev**
 
 v1.0
 ~~~~
