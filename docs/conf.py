3615c1ab1160e7b9b61997bb0a81950aecc92e02
Aniruddha Maru <maru@standard.ai>
2020-09-04 15:36:13 -0700

    Bump version: 8.3.0 → 8.3.1-dev

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = 'Automatic Labs'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '8.3.0'
+release = '8.3.1-dev'
 
 
 # -- General configuration ---------------------------------------------------
