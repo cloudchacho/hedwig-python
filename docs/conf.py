4781150584fbcf3100d11c0f5b53faff012b8389
Aniruddha Maru <maru@standard.ai>
2020-09-16 09:52:01 -0700

    Bump version: 8.5.0-dev → 8.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = 'Automatic Labs'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '8.5.0-dev'
+release = '8.5.0'
 
 
 # -- General configuration ---------------------------------------------------
--- docs/releases.rst
+++ docs/releases.rst
@@ -1,7 +1,7 @@
 Release Notes
 =============
 
-**Current version: v8.5.0-dev**
+**Current version: v8.5.0**
 
 v1.0
 ~~~~
