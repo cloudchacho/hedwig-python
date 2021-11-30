e2c81458930074a410ff94683cc77e1a1886a0e5
Aniruddha Maru <maru@standard.ai>
2021-11-30 22:04:08 +0530

    fix tests

--- tests/test_instrumentation/conftest.py
+++ tests/test_instrumentation/conftest.py
@@ -1,13 +1,31 @@
+from distutils.version import StrictVersion
+
 import pytest
 
 
 @pytest.fixture(autouse=True, scope="session")
 def setup_instrumentation():
     from hedwig.instrumentation.compat import set_global_textmap
+    import opentelemetry.trace
     from opentelemetry.sdk.trace import TracerProvider
     from opentelemetry.trace import set_tracer_provider
     from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
+    from opentelemetry.version import __version__
 
-    set_tracer_provider(TracerProvider())
+    VERSION_1_6_0 = StrictVersion("1.6.0")
 
-    set_global_textmap(TraceContextTextMapPropagator())
+    try:
+        # might already be set to default tracer provider
+        opentelemetry.trace._TRACER_PROVIDER = None
+        if StrictVersion(__version__) >= VERSION_1_6_0:
+            opentelemetry.trace._TRACER_PROVIDER_SET_ONCE._done = False
+
+        set_tracer_provider(TracerProvider())
+
+        set_global_textmap(TraceContextTextMapPropagator())
+
+        yield
+    finally:
+        opentelemetry.trace._TRACER_PROVIDER = None
+        if StrictVersion(__version__) >= VERSION_1_6_0:
+            opentelemetry.trace._TRACER_PROVIDER_SET_ONCE._done = False
