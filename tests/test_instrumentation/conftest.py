46a2b740acd7ded930af9f7c2589c27a758e3e6b
Aniruddha Maru <maru@standard.ai>
2021-11-30 21:33:38 +0530

    Bump requirements

--- tests/test_instrumentation/conftest.py
+++ tests/test_instrumentation/conftest.py
@@ -1,22 +1,13 @@
 import pytest
 
 
-@pytest.fixture(autouse=True, scope="module")
+@pytest.fixture(autouse=True, scope="session")
 def setup_instrumentation():
     from hedwig.instrumentation.compat import set_global_textmap
-    import opentelemetry.trace
     from opentelemetry.sdk.trace import TracerProvider
     from opentelemetry.trace import set_tracer_provider
     from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 
-    try:
-        # might already be set to default tracer provider
-        opentelemetry.trace._TRACER_PROVIDER = None
+    set_tracer_provider(TracerProvider())
 
-        set_tracer_provider(TracerProvider())
-
-        set_global_textmap(TraceContextTextMapPropagator())
-
-        yield
-    finally:
-        opentelemetry.trace._TRACER_PROVIDER = None
+    set_global_textmap(TraceContextTextMapPropagator())
--- tests/test_validators/test_protobuf.py
+++ tests/test_validators/test_protobuf.py
@@ -334,7 +334,7 @@ class TestProtobufValidator:
             self._validator().deserialize(payload, attrs, None)
         assert e.value.args[0] in [
             'Invalid data for message: TripCreatedV1: Error parsing message',
-            'Invalid data for message: TripCreatedV1: Wrong wire type in tag.',
+            "Invalid data for message: TripCreatedV1: Error parsing message with type 'tests.TripCreatedV1'",
         ]
 
     def test_serialize_no_error_invalid_data(self):
