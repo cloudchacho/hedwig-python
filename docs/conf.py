70b7c55cd9934f84a578b1db7f4893be0f14770e
Aniruddha Maru <aniruddhamaru@gmail.com>
2022-12-12 13:05:48 +0530

    Bump version: 9.0.2-dev → 9.1.0-dev

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ author = 'Cloudchacho'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '9.0.2-dev'
+release = '9.1.0-dev'
 
 
 # -- General configuration ---------------------------------------------------
--- docs/releases.rst
+++ docs/releases.rst
@@ -1,7 +1,7 @@
 Release Notes
 =============
 
-**Current version: v9.0.2-dev**
+**Current version: v9.1.0-dev**
 
 v1.0
 ~~~~
