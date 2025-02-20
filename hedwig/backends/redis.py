95648785c099d50a2668b6cacc39485e8e0cedc2
Bakowski, Marcin <marcin.bakowski@standard.ai>
2025-02-20 14:37:30 +0100

    add tests

--- hedwig/backends/redis.py
+++ hedwig/backends/redis.py
@@ -4,9 +4,8 @@ import threading
 import uuid
 from concurrent.futures import Future
 from typing import Union, Dict, Optional, Generator, List, Tuple
-from urllib.parse import urlparse
 
-import redis
+from redis import Redis
 
 from hedwig.backends.base import HedwigPublisherBaseBackend, HedwigConsumerBaseBackend
 from hedwig.conf import settings
@@ -14,9 +13,15 @@ from hedwig.models import Message
 
 
 def _client():
-    uri = settings.REDIS_URI
-    parsed = urlparse(uri)
-    return redis.Redis(host=parsed.hostname, port=int(parsed.port), db=parsed.path.lstrip("/"), protocol=3)
+    return Redis.from_url(settings.REDIS_URI, protocol=3)
+
+
+@dataclasses.dataclass(frozen=True)
+class RedisMessage:
+    stream: bytes
+    key: bytes
+    payload: dict[bytes, bytes]
+    delivery_attempt: int
 
 
 @dataclasses.dataclass(frozen=True)
@@ -43,8 +48,16 @@ class RedisStreamsPublisherBackend(HedwigPublisherBaseBackend):
     def __init__(self) -> None:
         self._r = _client()
 
-    def _mock_queue_message(self, message: Message):
-        raise NotImplementedError
+    def _mock_queue_message(self, message: Message) -> RedisMessage:
+        payload, attributes = message.serialize()
+        if isinstance(payload, bytes):
+            payload = base64.encodebytes(payload).decode()
+            attributes['hedwig_encoding'] = 'base64'
+        redis_message = {"hedwig_payload": payload, **attributes}
+        redis_message = {k.encode(): v.encode() for k, v in redis_message.items()}
+        stream = f"hedwig:{self.topic(message)}".encode()
+        message_id = f"{attributes['hedwig_message_timestamp']}-0".encode()
+        return RedisMessage(stream=stream, key=message_id, payload=redis_message, delivery_attempt=1)
 
     def _publish(self, message: Message, payload: Union[str, bytes], attributes: Dict[str, str]) -> Union[str, Future]:
         key = f"hedwig:{self.topic(message)}"
@@ -69,17 +82,17 @@ class RedisStreamsConsumerBackend(HedwigConsumerBaseBackend):
         self._group = settings.HEDWIG_QUEUE
         self._consumer_id = str(uuid.uuid4())
         self._main_stream = f"hedwig:{settings.HEDWIG_QUEUE}"
-        self._dlq_stream = f"hedwig:{settings.HEDWIG_QUEUE}:dlq"
+        self._deadletter_stream = f"hedwig:{settings.HEDWIG_QUEUE}:dlq"
         if dlq:
-            self._streams = [self._dlq_stream]
+            self._streams = [self._deadletter_stream]
         else:
             self._streams = [f"hedwig:{x}" for x in settings.HEDWIG_SUBSCRIPTIONS]
             # main queue for DLQ re-queued messages
             self._streams.append(self._main_stream)
         super().__init__()
 
-    def message_attributes(self, queue_message) -> dict:
-        return {k.decode(): v.decode() for k, v in queue_message[2].items()}
+    def message_attributes(self, queue_message: RedisMessage) -> dict:
+        return {k.decode(): v.decode() for k, v in queue_message.payload.items()}
 
     def extend_visibility_timeout(self, visibility_timeout_s: int, metadata) -> None:
         assert visibility_timeout_s == settings.HEDWIG_VISIBILITY_TIMEOUT_S, "Visibility timeout is not configurable"
@@ -97,11 +110,11 @@ class RedisStreamsConsumerBackend(HedwigConsumerBaseBackend):
         total_count = 0
         while True:
             dlq_entries: Tuple[str, List[Tuple[str, Dict]], List] = self._r.xautoclaim(
-                self._dlq_stream,
-                self._group,
-                self._consumer_id,
-                0,
-                "0-0",
+                name=self._deadletter_stream,
+                groupname=self._group,
+                consumername=self._consumer_id,
+                min_idle_time=0,
+                start_id="0-0",
                 count=num_messages,
             )
             if not dlq_entries:
@@ -119,7 +132,7 @@ class RedisStreamsConsumerBackend(HedwigConsumerBaseBackend):
             entries: Dict[str, List[List[Tuple[str, Dict]]]] = self._r.xreadgroup(
                 self._group,
                 self._consumer_id,
-                streams={self._dlq_stream: ">"},
+                streams={self._deadletter_stream: ">"},
                 count=num_messages,
                 block=500,
             )
@@ -149,21 +162,28 @@ class RedisStreamsConsumerBackend(HedwigConsumerBaseBackend):
             for message in messages:
                 message_ids.append(message[0])
                 pipeline.xadd(self._main_stream, message[1])
-            pipeline.xack(self._dlq_stream, self._group, *message_ids)
+            pipeline.xack(self._deadletter_stream, self._group, *message_ids)
             pipeline.execute()
 
-    def _process_raw_messages(self, stream, messages):
+    def _process_raw_messages(
+        self, stream: bytes, messages: list[tuple[bytes, dict[bytes, bytes]]]
+    ) -> Generator[RedisMessage, None, None]:
         for message in messages:
-            message_id = message[0].decode()
+            message_id, message_payload = message
             # manual tracking of delivery attempts
             metadata = self._r.xpending_range(stream, self._group, message_id, message_id, 1)[0]
             if metadata["times_delivered"] > settings.HEDWIG_MAX_DELIVERY_ATTEMPTS:
                 with self._r.pipeline() as pipeline:
-                    pipeline.xadd(self._dlq_stream, message[1])
+                    pipeline.xadd(self._deadletter_stream, message_payload)
                     pipeline.xack(stream, self._group, message_id)
                     pipeline.execute()
                 continue
-            yield stream, *message, metadata["times_delivered"]
+            yield RedisMessage(
+                stream=stream,
+                key=message_id,
+                payload=message_payload,
+                delivery_attempt=metadata["times_delivered"],
+            )
 
     def pull_messages(  # type: ignore[return]
         self,
@@ -176,15 +196,15 @@ class RedisStreamsConsumerBackend(HedwigConsumerBaseBackend):
             # claim timed out messages
             for stream in self._streams:
                 stream_entries = self._r.xautoclaim(
-                    stream,
-                    self._group,
-                    self._consumer_id,
-                    settings.HEDWIG_VISIBILITY_TIMEOUT_S * 1000,
-                    "0-0",
+                    name=stream,
+                    groupname=self._group,
+                    consumername=self._consumer_id,
+                    min_idle_time=int(settings.HEDWIG_VISIBILITY_TIMEOUT_S * 1000),
+                    start_id="0-0",
                     count=num_messages,
                 )[1]
                 yield from self._process_raw_messages(stream.encode(), stream_entries)
-            entries: dict = self._r.xreadgroup(
+            entries: dict[bytes, list[list[tuple[bytes, dict[bytes, bytes]]]]] = self._r.xreadgroup(
                 groupname=self._group,
                 consumername=self._consumer_id,
                 streams={x: ">" for x in self._streams},
@@ -193,34 +213,30 @@ class RedisStreamsConsumerBackend(HedwigConsumerBaseBackend):
             )
             if not entries:
                 return []
-            for stream, stream_entries in entries.items():
+            for stream_, stream_entries in entries.items():
                 for messages in stream_entries:
-                    yield from self._process_raw_messages(stream, messages)
+                    yield from self._process_raw_messages(stream_, messages)
         finally:
             self._perform_error_counter_inactivity_reset()
             self._call_heartbeat_hook()
 
-    def process_message(self, queue_message) -> None:
-        stream = queue_message[0].decode()
-        message_id = queue_message[1].decode()
-        fields = {k.decode(): v.decode() for k, v in queue_message[2].items()}
-        delivery_attempt = queue_message[3]
+    def process_message(self, queue_message: RedisMessage) -> None:
+        stream = queue_message.stream.decode()
+        message_id = queue_message.key.decode()
+        fields = self.message_attributes(queue_message)
         # body is always UTF-8 string
         message_payload = fields.pop("hedwig_payload")
         if fields.pop("hedwig_encoding", None) == "base64":
             message_payload = base64.decodebytes(message_payload.encode())
-        receipt = message_id
         self.message_handler(
             message_payload,
             fields,
-            RedisMetadata(receipt, stream, delivery_attempt),
+            RedisMetadata(message_id, stream, queue_message.delivery_attempt),
         )
 
-    def ack_message(self, queue_message) -> None:
-        stream = queue_message[0]
-        message_id = queue_message[1]
-        self._r.xack(stream, self._group, message_id)
+    def ack_message(self, queue_message: RedisMessage) -> None:
+        self._r.xack(queue_message.stream, self._group, queue_message.key)
 
-    def nack_message(self, queue_message) -> None:
+    def nack_message(self, queue_message: RedisMessage) -> None:
         # let visibility timeout take care of it
         pass
